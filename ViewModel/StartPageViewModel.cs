abba04ba9f21198b85d18b2f1f0bd5e49dec0b41
justkiwon <kkhiddd@gmail.com>
2023-02-19 22:41:30 +0900

    ViewModel까지 연결 완료

---
+++ ViewModel/StartPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Equipment_management.ViewModel
+{
+    public class StartPageViewModel
+    {
+        //Console.WriteLine("sds");
+        int a = 0;
+    }
+}
